37e2ed25efd8b594107d7beffaab507d23ac6377
Vishwas <vishwaszadte@gmail.com>
2026-09-20 16:55:08 +0530

    feat: modernize portfolio with Astro, PostHog-inspired design, and updated career story

--- src/script.js
+++
@@ -1,8 +0,0 @@
-const hamburgerBtn = document.getElementById("hamburger");
-const navList = document.getElementById("navList");
-
-function toggleBtn() {
-    navList.classList.toggle("show");
-}
-
-hamburgerBtn.addEventListener("click", toggleBtn);
